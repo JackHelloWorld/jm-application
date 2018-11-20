a01a9ea84047ea7b736ae24abf01b22ae7ef50d8
jack <190986473@qq.com>
2018-11-20 14:13:06 +0800

    修改api为本地

--- jm-admin-html/plugins/config.js
+++ jm-admin-html/plugins/config.js
@@ -6,78 +6,78 @@ var config = {
 var urlConfig = {
 	
 	//登录
-	login : 'http://192.168.2.101:9999/user/login',
+	login : 'http://127.0.0.1:9999/user/login',
 	
 	//获取系统分配session
-	getsessionid : 'http://192.168.2.101:9999/user/login/code/initsession',
+	getsessionid : 'http://127.0.0.1:9999/user/login/code/initsession',
 	
 	//登录验证码
-	loginCode : 'http://192.168.2.101:9999/user/login/code',
+	loginCode : 'http://127.0.0.1:9999/user/login/code',
 	//获得菜单资源信息
-	getMenus : 'http://192.168.2.101:9999/home/find/menus',
+	getMenus : 'http://127.0.0.1:9999/home/find/menus',
 	//搜索菜单
-	searchMenus : 'http://192.168.2.101:9999/home/search/menus',
+	searchMenus : 'http://127.0.0.1:9999/home/search/menus',
 	//获得个人信息
-	getPersonalInfo : 'http://192.168.2.101:9999/user/get/info',
+	getPersonalInfo : 'http://127.0.0.1:9999/user/get/info',
 	//退出登录
-	userExit : 'http://192.168.2.101:9999/user/logout',
+	userExit : 'http://127.0.0.1:9999/user/logout',
 	//修改密码
-	updatePassword : 'http://192.168.2.101:9999/user/update/password',
+	updatePassword : 'http://127.0.0.1:9999/user/update/password',
 	
 	//根据菜单获得功能资源信息
-	getAction : 'http://192.168.2.101:9999/home/find/action',
+	getAction : 'http://127.0.0.1:9999/home/find/action',
 	
 	common : {
 		
 		//获得地址信息
-		getAddress : 'http://192.168.2.101:9999/common/get/address',
+		getAddress : 'http://127.0.0.1:9999/common/get/address',
 		//图片上传地址
-		imgUpload : 'http://192.168.2.101:9999/common/upload/img',
+		imgUpload : 'http://127.0.0.1:9999/common/upload/img',
 		//文件上传地址
-		fileUpload : 'http://192.168.2.101:9999/common/upload/file',
+		fileUpload : 'http://127.0.0.1:9999/common/upload/file',
 		//修改头像
-		updateProfile : 'http://192.168.2.101:9999/common/update_profile',
+		updateProfile : 'http://127.0.0.1:9999/common/update_profile',
 	},
 	
 	/**resource.html*/
 	resource : {
-		findAllMenus : 'http://192.168.2.101:9999/resource/find',
-		saveResource : 'http://192.168.2.101:9999/resource/save',
-		updateResource : 'http://192.168.2.101:9999/resource/update',
-		deleteResource : 'http://192.168.2.101:9999/resource/delete',
+		findAllMenus : 'http://127.0.0.1:9999/resource/find',
+		saveResource : 'http://127.0.0.1:9999/resource/save',
+		updateResource : 'http://127.0.0.1:9999/resource/update',
+		deleteResource : 'http://127.0.0.1:9999/resource/delete',
 	},
 	
 	/**dictionary.html*/
 	dictionary : {
-		findAllDictionary : 'http://192.168.2.101:9999/dictionary/find',
-		saveDictionary : 'http://192.168.2.101:9999/dictionary/save',
-		updateDictionary : 'http://192.168.2.101:9999/dictionary/update',
-		deleteDictionary : 'http://192.168.2.101:9999/dictionary/delete',
+		findAllDictionary : 'http://127.0.0.1:9999/dictionary/find',
+		saveDictionary : 'http://127.0.0.1:9999/dictionary/save',
+		updateDictionary : 'http://127.0.0.1:9999/dictionary/update',
+		deleteDictionary : 'http://127.0.0.1:9999/dictionary/delete',
 	},
 	
 	/**role.html*/
 	role : {
-		list : 'http://192.168.2.101:9999/role/list',
-		updateInfo : 'http://192.168.2.101:9999/role/update/info',
-		update : 'http://192.168.2.101:9999/role/update',
-		save : 'http://192.168.2.101:9999/role/save',
-		deleteInfo : 'http://192.168.2.101:9999/role/delete',
-		resource : 'http://192.168.2.101:9999/role/resource',
-		findResource : 'http://192.168.2.101:9999/role/find/resource',
-		block : 'http://192.168.2.101:9999/role/block',
-		success : 'http://192.168.2.101:9999/role/success',
+		list : 'http://127.0.0.1:9999/role/list',
+		updateInfo : 'http://127.0.0.1:9999/role/update/info',
+		update : 'http://127.0.0.1:9999/role/update',
+		save : 'http://127.0.0.1:9999/role/save',
+		deleteInfo : 'http://127.0.0.1:9999/role/delete',
+		resource : 'http://127.0.0.1:9999/role/resource',
+		findResource : 'http://127.0.0.1:9999/role/find/resource',
+		block : 'http://127.0.0.1:9999/role/block',
+		success : 'http://127.0.0.1:9999/role/success',
 	},
 	
 	/**webuser.html*/
 	webuser : {
-		list : 'http://192.168.2.101:9999/webuser/list',
-		updateInfo : 'http://192.168.2.101:9999/webuser/update/info',
-		update : 'http://192.168.2.101:9999/webuser/update',
-		save : 'http://192.168.2.101:9999/webuser/save',
-		deleteInfo : 'http://192.168.2.101:9999/webuser/delete',
-		findRoles : 'http://192.168.2.101:9999/webuser/find/roles',
-		block : 'http://192.168.2.101:9999/webuser/block',
-		success : 'http://192.168.2.101:9999/webuser/success',
+		list : 'http://127.0.0.1:9999/webuser/list',
+		updateInfo : 'http://127.0.0.1:9999/webuser/update/info',
+		update : 'http://127.0.0.1:9999/webuser/update',
+		save : 'http://127.0.0.1:9999/webuser/save',
+		deleteInfo : 'http://127.0.0.1:9999/webuser/delete',
+		findRoles : 'http://127.0.0.1:9999/webuser/find/roles',
+		block : 'http://127.0.0.1:9999/webuser/block',
+		success : 'http://127.0.0.1:9999/webuser/success',
 	},
 	
 }
