5eb5ceedaf9c746c30ed1d07a5ab33424ee99a4f
jack <190986473@qq.com>
2018-12-17 17:23:08 +0800

    api配置

--- jmsoft-admin/src/main/java/com/jmsoft/SwaggerConfiguration.java
+++ jmsoft-admin/src/main/java/com/jmsoft/SwaggerConfiguration.java
@@ -2,29 +2,31 @@ package com.jmsoft;
 
 import org.springframework.context.annotation.Bean;
 
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
+/**
+ * api配置
+ * @author Jack
+ *
+ */
 public class SwaggerConfiguration {
 	
-	/**
-	 * 通过 createRestApi函数来构建一个DocketBean
-	 * 函数名,可以随意命名,喜欢什么命名就什么命名
-	 */
 	@Bean
 	public Docket createRestApi() {
 		return new Docket(DocumentationType.SWAGGER_2)
 				.apiInfo(apiInfo())
 				.select()
-				//控制暴露出去的路径下的实例
-				//如果某个接口不想暴露,可以使用以下注解
-				//@ApiIgnore 这样,该接口就不会暴露在 swagger2 的页面下
 				.apis(RequestHandlerSelectors.basePackage("com.jmsoft"))
 				.paths(PathSelectors.any())
 				.build();
 	}
 	
-    //构建 api文档的详细信息函数
+    //构建 api文档的详细信息函数	
 	private ApiInfo apiInfo() {
 		return new ApiInfoBuilder()
 				//页面标题
